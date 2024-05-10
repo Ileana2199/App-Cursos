3658be37bdf5eb8979e429f0d564bfa958625916
ileana2199 <ileanapintalucas@gmail.com>
2024-05-10 00:01:51 -0500

    Proyecto alumnos-cursos

---
+++ secciones/index.php
@@ -0,0 +1,15 @@
+<?php include('../templates/cabecera.php');  ?>
+
+<div class="p-5 mb-4 bg-light rounded-3">
+    <div class="container-fluid py-5">
+      <h1 class="display-5 fw-bold">Bienvenido a la aplicación de certificados</h1>
+      <p class="col-md-8 fs-4">Genere certificados a la medida</p>
+      <hr class="my-2">
+      <p>Iniciar la aplicación</p>
+      <a class="btn btn-primary btn-lg" href="vista_alumnos.php" type="button">Iniciar</a>
+    </div>
+  </div>
+
+<?php include('../templates/pie.php'); ?>
+
+
